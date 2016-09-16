64fc7ef4865d84c0025a4be7952fdafdd30441ab
Job Snijders <job@instituut.net>
2016-09-16 13:29:33 +0200

    add docs for large

--- lib/exabgp/bgp/message/update/attribute/community/common/large/community.py
+++ lib/exabgp/bgp/message/update/attribute/community/common/large/community.py
@@ -12,8 +12,52 @@ from exabgp.bgp.message.update.attribute import Attribute
 from struct import pack
 from struct import unpack
 
+"""
+
+The "Global Administrator" comes from the Community Common Header, packing is
+as follows:
+
+    192.0.2.0/24 2914:444:555 2914:555:999 3356:33:1
+
+    should be packed as:
+
+        0                   1                   2                   3
+        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     / |     Type = Large Community    |          Length = 2 * 8       |
+   CH  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     \ |                             2914				               |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     / |                              444                              |
+   LC  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     \ |                              555                              |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     / |                              555                              |
+   LC  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     \ |                              999                              |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+followed by a new path attribute with common header + large community:
+
+        0                   1                   2                   3
+        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     / |     Type = Large Community    |          Length = 2 * 8       |
+   CH  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     \ |                             3356                              |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     / |                              33                               |
+   LC  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     \ |                               1                               |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+"""
+
 class LargeCommunity (Attribute):
-	MAX = 0xFFFFFFFFFFFFFFFFFFFFFFFF
+	MAX = 0xFFFFFFFFFFFFFFFF
+
+    # must come from common header
+    self.GLOBAL_ADMINISTRATOR = 0x0
 
 	cache = {}
 	caching = True
@@ -22,7 +66,7 @@ class LargeCommunity (Attribute):
 
 	def __init__ (self, large_community):
 		self.large_community = large_community
-		self._str = "%d:%d:%d" % unpack('!LLL', self.large_community)
+		self._str = "%d:%d" % unpack('!LL', self.large_community)
 
 	def __eq__ (self, other):
 		return self.large_community == other.large_community
@@ -43,7 +87,8 @@ class LargeCommunity (Attribute):
 		return self.large_community >= other.large_community
 
 	def json (self):
-		return "[ %d, %d , %d ]" % unpack('!LLL', self.large_community)
+		return "[ %d, %d , %d ]" \
+			% (self.GLOBAL_ADMINISTRATOR, unpack('!LL', self.large_community))
 
 	def pack (self, negotiated=None):
 		return self.large_community
